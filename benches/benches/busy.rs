ad3b517186af1d0930535ed585746a637cbd8ed9
Pedro Fedricci <pedromfedricci@gmail.com>
2023-12-14 16:28:47 -0300

    Remove benches for now

--- benches/benches/busy.rs
+++
@@ -1,8 +0,0 @@
-use benches::generate_bench;
-
-generate_bench! {
-    "create-mcslock-busy",
-    "lock_unlock-mcslock-busy",
-    "lock_unlock_read_contention-mcslock-busy",
-    "lock_unlock_write_contention-mcslock-busy"
-}
--- benches/benches/yield.rs
+++
@@ -1,8 +0,0 @@
-use benches::generate_bench;
-
-generate_bench! {
-    "create-mcslock-yield",
-    "lock_unlock-mcslock-yield",
-    "lock_unlock_read_contention-mcslock-yield",
-    "lock_unlock_write_contention-mcslock-yield"
-}
